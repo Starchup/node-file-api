2984b8a4f51a25f46410cead17ea2b2bd4d1e05b
Geoffroy Lesage <gefthefrench@gmail.com>
2019-05-31 20:27:18 +0200

    Read 10x/second

--- app/classes/FileWatcher.ts
+++ app/classes/FileWatcher.ts
@@ -28,7 +28,7 @@ export class FileWatcher
             {
                 console.error('FileWatcher ' + fileName + ' got error: ' + err.toString());
             });
-        }, 1000);
+        }, 100);
     }
 
     /* Private helpers */
